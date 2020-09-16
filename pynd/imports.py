a1e2648b566d1115c5a21397ad564f0ca1e98fca
Owen <owen.allemang@gmail.com>
2020-09-17 00:10:45 +0100

    added imports

--- pynd/imports.py
+++ pynd/imports.py
@@ -1,2 +1,27 @@
+from pynd.constants import *
 
+from pynd.dice.dice import *
 
+from pynd.dobject.attack import *
+from pynd.dobject.character import *
+from pynd.dobject.dobject import *
+from pynd.dobject.item import *
+from pynd.dobject.race import *
+from pynd.dobject.spell import *
+from pynd.dobject.weapon import *
+
+from pynd.utils.config_ops import *
+from pynd.utils.strings import *
+
+import inspect
+import itertools as it
+import numpy as np
+import operator as op
+import os
+import random
+import re
+import string
+import sys
+import math
+
+from colour import Color
