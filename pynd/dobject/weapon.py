988e54480c1f1d3a45b6e3c4179faf87ee32240e
Owen <owen.allemang@gmail.com>
2020-09-17 00:09:47 +0100

    added bow and sword

--- pynd/dobject/weapon.py
+++ pynd/dobject/weapon.py
@@ -3,8 +3,28 @@ from pynd.dobject.dobject import Dobject
 
 class Weapon(Dobject):
     CONFIG = {
-
+        "use_ammo": False,
+        "ranged": False,
     }
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+
+class Sword(Weapon):
+    CONFIG = {
+    }
+
+    def __init__(self, weapon, **kwargs):
+        Weapon.__init__(self, **kwargs)
+
+
+class Bow(Weapon):
+    CONFIG = {
+        "use_ammo": True,
+        "ranged": True,
+        "ammo_type": None,
+    }
+
+    def __init__(self, weapon, **kwargs):
+        Weapon.__init__(self, **kwargs)
